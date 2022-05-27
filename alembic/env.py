e1dd04b9ffaabb9fb999499b3dffc0a16d472a23
Michael Shuff <mjshuff23@gmail.com>
2022-05-26 21:04:02 -0400

    figured out a way to seed

--- alembic/env.py
+++ alembic/env.py
@@ -29,7 +29,6 @@ config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
